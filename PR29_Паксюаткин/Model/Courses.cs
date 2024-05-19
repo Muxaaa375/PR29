557b4196a2ae68820b55b0fee8a6f4b51e8d4356
20rwplay@gmail.com <20rwplay@gmail.com>
2024-05-19 18:05:12 +0500

    Разработанный класс Courses в папке Model. Паксюаткин

---
+++ PR29_Паксюаткин/Model/Courses.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace PR29_Паксюаткин.Model
+{
+    public class Courses : INotifyPropertyChanged
+    {
+
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                OnPropertyChanged("_id");
+            }
+        }
+        private string? _title;
+        public string? Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("_title");
+            }
+        }
+        private DateTime _date;
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+                OnPropertyChanged("_date");
+            }
+        }
+
+        private TimeSpan _time;
+        public TimeSpan Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                OnPropertyChanged("_time");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }     
+
+    }
+}
